4460b2af249020c47de15adce349b6d191622c1f
nivin77789 <nivin77789@gmail.com>
2023-07-21 14:21:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Main from "./components/main";
+import Side from "./components/side";
+import Taskbar from "./components/taskbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Taskbar />
+      <div className="sub">
+        <Side />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Main />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
