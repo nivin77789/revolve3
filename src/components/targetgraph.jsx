81d0979e891d95df0f77736430654950938b0dbc
nivin77789 <nivin77789@gmail.com>
2023-07-21 15:17:45 +0530

    adsf

--- src/components/targetgraph.jsx
+++ src/components/targetgraph.jsx
@@ -3,51 +3,51 @@ import { ResponsiveBar } from "@nivo/bar";
 const data = [
   {
     country: "Mon",
-    "online sales": 51,
+    "Reality sales": 51,
     "hot dogColor": "hsl(310, 70%, 50%)",
-    "offline sales": 91,
+    "Target sales": 91,
     burgerColor: "hsl(265, 70%, 50%)",
   },
   {
     country: "Tue",
-    "online sales": 199,
+    "Reality sales": 199,
     "hot dogColor": "hsl(239, 70%, 50%)",
-    "offline sales": 193,
+    "Target sales": 193,
     burgerColor: "hsl(213, 70%, 50%)",
   },
   {
     country: "Wed",
-    "online sales": 43,
+    "Reality sales": 43,
     "hot dogColor": "hsl(317, 70%, 50%)",
-    "offline sales": 120,
+    "Target sales": 120,
     burgerColor: "hsl(26, 70%, 50%)",
   },
   {
     country: "Thu",
-    "online sales": 167,
+    "Reality sales": 167,
     "hot dogColor": "hsl(128, 70%, 50%)",
-    "offline sales": 92,
+    "Target sales": 92,
     burgerColor: "hsl(331, 70%, 50%)",
   },
   {
     country: "Fri",
-    "online sales": 192,
+    "Reality sales": 192,
     "hot dogColor": "hsl(49, 70%, 50%)",
-    "offline sales": 176,
+    "Target sales": 176,
     burgerColor: "hsl(118, 70%, 50%)",
   },
   {
     country: "Sat",
-    "online sales": 61,
+    "Reality sales": 61,
     "hot dogColor": "hsl(274, 70%, 50%)",
-    "offline sales": 68,
+    "Target sales": 68,
     burgerColor: "hsl(94, 70%, 50%)",
   },
   {
     country: "Sun",
-    "online sales": 161,
+    "Reality sales": 161,
     "hot dogColor": "hsl(329, 70%, 50%)",
-    "offline sales": 14,
+    "Target sales": 14,
     burgerColor: "hsl(133, 70%, 50%)",
   },
 ];
@@ -55,7 +55,7 @@ const Targetgraph = () => {
   return (
     <ResponsiveBar
       data={data}
-      keys={["online sales", "offline sales"]}
+      keys={["Reality sales", "Target sales"]}
       indexBy="country"
       margin={{ top: 5, right: 10, bottom: 90, left: 10 }}
       padding={0.25}
